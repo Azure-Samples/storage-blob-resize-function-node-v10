69da6f8bbf2ab28ca3605af5ab2ba6db910b3c05
Karl Erickson <karler@microsoft.com>
2019-02-05 14:58:01 -0800

    added sample

--- Thumbnail/index.js
+++ Thumbnail/index.js
@@ -1,41 +1,3 @@
-# Azure Storage Blob Trigger Image Resize Function in Node.js using the v10 SDK
-
-This sample implements a function triggered by Azure Blob Storage to resize an image in Node.js. Once the image is resized, the thumbnail image is uploaded back to blob storage.
-
-The key aspects of this sample are in the function bindings and implementation.
-
-## Function bindings
-
-In order to interface with image data, you need to configure the function to process binary data.
-
-The following code sets the `datatype` parameter to `binary` in the `function.json` file.
-
-```javascript
-{
-  "disabled": false,
-  "bindings": [
-    {
-      "type": "eventGridTrigger",
-      "name": "eventGridEvent",
-      "direction": "in"
-    },
-    {
-      "type": "blob",
-      "name": "inputBlob",
-      "path": "{data.url}",
-      "connection": "AZURE_STORAGE_CONNECTION_STRING",
-      "direction": "in",
-      "datatype": "binary"
-    }
-  ]
-}
-```
-
-## Function implementation
-
-The sample uses [Jimp](https://github.com/oliver-moran/jimp) to resize an incoming buffer to a thumbnail. The buffer is then converted to a stream and uploaded to Azure Storage.
-
-```javascript
 const stream = require('stream');
 const Jimp = require('jimp');
 
@@ -108,6 +70,3 @@ module.exports = (context, eventGridEvent, inputBlob) => {
     });
   });
 };
-```
-
-You can use the [Azure Storage Explorer](https://azure.microsoft.com/features/storage-explorer/) to view blob containers and verify the resize is successful.
